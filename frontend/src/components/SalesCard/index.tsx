31e1f508cb388ad2d67b299e78327509c9bed127
ANTONIO CARLOS <acsmcarlos@gmail.com>
2022-07-15 14:57:14 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -22,11 +22,17 @@ function SalesCard() {
 
   // Buscanddo no backend as vendas
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`)
+
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+
+    console.log(dmin);
+
+    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
         .then(response => {
             setSales(response.data.content);
         });
-  }, []);
+  }, [minDate, maxDate]);
 
   return (
     <div className="dsmeta-card">
